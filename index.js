c58562ad1c5e0d678ee14112192d884c4284e8f0
shahzoda <istamovashahzoda04@gmail.com>
2023-03-09 22:03:39 +0300

    commits

---
+++ index.js
@@ -0,0 +1,29 @@
+"use script"
+
+const user = {
+  fName:"Shakhzoda",
+  login : "@gmail.com",
+  pass:"57163204Mu"
+};
+
+const amin = {
+  fName:"admin",
+  login : "@admin.com",
+  pass:"admin"
+}
+
+const costumer = {
+  fName:"costumer",
+  login : "@admin.com",
+  pass:"costumer"
+}
+
+const arr = [amin,costumer,user];
+
+for(let  i = 0; i < arr.length; i ++){
+  //console.log(arr[i].pass);
+
+  if(arr[i].costumer =="user"){
+    //console.log(arr[i]);
+  }
+}
